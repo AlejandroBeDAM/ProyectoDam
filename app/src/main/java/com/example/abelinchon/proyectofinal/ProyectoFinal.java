8c6bc943de7e835cf417110635e51a7f22047f55
Alejandro Belinchon Ahijon <abelinchon@sia.es>
2017-03-28 15:49:53 +0200

    'Proyecto'

---
+++ app/src/main/java/com/example/abelinchon/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,35 @@
+package com.example.abelinchon.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class ProyectoFinal extends AppCompatActivity {
+    private TabHost tabHost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto_final);
+        inicializarTab();
+    }
+
+    private void inicializarTab() {
+        tabHost = (TabHost)findViewById(R.id.tabhost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec = tabHost.newTabSpec("Tab uno");
+        spec.setContent(R.id.Peliculas);
+        spec.setIndicator("Peliculas");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("Tab dos");
+        spec.setContent(R.id.Series);
+        spec.setIndicator("Series");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("Tab tres");
+        spec.setContent(R.id.Anime);
+        spec.setIndicator("Anime");
+        tabHost.addTab(spec);
+    }
+}
